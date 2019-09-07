c7aacf1995254df31ef71df1af0aff69cc76e76e
Piotrek <unknow@gmail.com>
2019-09-07 16:03:40 +0200

    Add Models, DbContext and library EFCore Sqlite

---
+++ PortalDating.API/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalDating.API.Models;
+
+namespace PortalDating.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
--- PortalDating.API/Startup.cs
+++ PortalDating.API/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PortalDating.API.Data;
 
 namespace PortalDating.API
 {
@@ -23,6 +25,7 @@ namespace PortalDating.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite("ConnectionString"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
