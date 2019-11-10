dc3f244452a7162af67d255bba0dce3aa439f241
Piotrek <unknow@gmail.com>
2019-11-10 20:58:35 +0100

    Dodano navigację.

--- PortalDating.SPA/src/app/app.module.ts
+++ PortalDating.SPA/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './Value/Value.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
